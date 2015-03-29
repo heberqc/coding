431018a63472c05caf10fe0a1671be34d0ee3f63
Heber U. Quequejana C <heberqc@gmail.com>
2015-03-29 14:37:14 -0500

    Agregar CookieClickerAlpha-C.c

---
+++ CookieClickerAlpha-C.c
@@ -0,0 +1,57 @@
+/*****************************************
+* NombrePrograma: CookieClickerAlpha.c
+* Descripcion: http://code.google.com/codejam/contest/2974486/dashboard#s=p1
+*
+* Autor: Heber Quequejana
+* Fecha: 
+*
+****************************************/
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#define MAX_FARMS 100
+
+int main(){
+	int T;
+	double *output;
+	scanf("%d",&T);
+	// matriz de datos
+	output = (double*) malloc(T * sizeof(double));
+	// input
+	for (int n = 0; n < T; n++){
+		// ingresar C, F y X
+		double C, F, X;
+		scanf("%lf %lf %lf",&C,&F,&X);
+		// variables
+		int farms = 0;
+		double ans = 0;
+		double ratio = 2.0;
+		bool done = false;
+		// calcular
+		while(!done && farms < MAX_FARMS){
+			double aux1 = X/ratio;
+			double aux2 = C/ratio;
+			double aux3 = aux2 + X/(ratio + F);
+			if (aux3 < aux1){
+				ans += aux2;
+				farms++;
+				ratio += F;
+			} else {
+				ans += aux1;
+				done = true;
+				output[n] = ans;
+			}
+		}
+	}
+	// output
+	printf("\n");
+	for (int i = 0; i < T; i++){
+		printf("Case #%d: %-.7f\n", (i+1), output[i]);
+	}
+	printf("\n");
+	system("pause");
+	return 0;
+}
